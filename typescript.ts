f127497709d9a06bf0e29a8518bc7cc1c780417a
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-07-10 13:51:35 +0545

    fixed pets: string[]

--- typescript.ts
+++ typescript.ts
@@ -11,9 +11,4 @@ let eyeColor: string = 'brown';
 let favoriteQuote: string = `I'm not old I am only the ${age}`
 
 //Array
-let pets: string[] = ['cat', 'dog', 1] /*
-typescript.ts:14:37 - error TS2322: Type 'number' is not assignable to type 'string'.
-
-14 let pets: string[] = ['cat', 'dog', 1]
-                                       ~
-*/
+let pets: string[] = ['cat', 'dog', 'pig']

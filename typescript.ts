4aecbe1615424e3d4919183bb66360c9d3989395
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-07-10 14:15:30 +0545

    only re-assignable to an object but not anything else

--- typescript.ts
+++ typescript.ts
@@ -13,3 +13,10 @@ let favoriteQuote: string = `I'm not old I am only the ${age}`
 //Array
 let pets: string[] = ['cat', 'dog', 'pig']
 let pets2: Array<string> = ['lion', 'dragon', 'lizard']
+
+// Object
+let wizard: object =  {
+    a: 'john'
+}
+
+wizard = Object; // only re-assignable to an object but not anything else
